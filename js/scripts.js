f776e44ad82fa3e394ef01a6074d25bdf15b3958
Mariyats7 <maryna.yatsushko@gmail.com>
2023-07-01 20:47:26 +0300

    section, owl

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            428:{
+                items:2
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+});
